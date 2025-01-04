21b4029d08e6526e3246e0468a3a6b3474cddc9a
sreedhar3210 <sreedhar.palampalli@gmail.com>
2025-01-04 07:52:11 +0530

    adjusted code, to adapt with React.js

--- backend/index.js
+++ backend/index.js
@@ -6,39 +6,27 @@ const createPostNode = require('./CreatePostNode');
 const app = express();
 
 const port = 8080;
+var curId = 0;
 var posts = [];
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../frontend')); // Optional if templates aren't in a `views` folder
-
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '../frontend/Home.html'));
-});
-
-app.get('/create-post', (req,res) => {
-    res.sendFile(path.join(__dirname, '../frontend/CreatePost.html'));
-});
 
 app.post('/create-post', (req,res) => {
     var postContent = req.body.PostContent;
-    // var postNode = createPostNode(postContent);
-    posts.push(postContent);
-    //console.log('post content data is being displayed from react html method as ', postContent);
-    res.redirect('/display-posts');
-});
-
-app.get('/display-posts', (req,res) => {
-    res.render(path.join(__dirname, '../frontend/DisplayPosts.ejs'), { posts });
+    posts.push({
+        "Id": ++curId,
+        "PostContent": postContent,
+        "NetScore": 0
+    });
+    console.log('PostContent is ', postContent);
+    console.log('posts are ', posts);
+    res.redirect('http://localhost:3000/display-posts');
 });
 
 app.get('/api/get-posts', (req, res) => {
-    const backendMsg = { "message" : "baaga pakadbandhi ga plan chesaav mike."};
-    res.send(backendMsg); // Send JSON response
+    
+    res.send(posts); // Send JSON response
 });
 
 
